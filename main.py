48d5b4eff47a59dfe7b6e9562cce998bd3d67113
JinxBlizzard <akashrajput.savvy@gmail.com>
2024-05-19 00:31:45 +0530

    Initial Code Push

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 from bs4 import BeautifulSoup
 import requests
+import time
 
-html_text = requests.get(
-    'https://internshala.com/internships/keywords-python/?utm_source=hp_internship_keyword_search').text
+anti_filter = input(">")
+print(f"Removing Skills : {anti_filter}")
 
-soup = BeautifulSoup(html_text, features="html.parser")
-jobs = soup.find_all('div', class_="internship_meta")
 
-for job in jobs:
-    get_time = job.find('div', class_="status status-small status-success")
-    get_company_name = job.find('a', class_="link_display_like_text")
-    get_job_type = job.find('div', class_="company")
-    if get_company_name is not None:
-        get_job_type = get_job_type.text.replace(' \n', '')
-        get_time = get_time.text.replace(' \n', '')
-        print(f"""
-        company name = {get_company_name.text}
-        job type = {get_job_type}
-        time posted = {get_time}
-        """)
-    # get_name = job.find('i', class_="ic-16-reschedule")
-# <div class="status status-small status-info"><i class="ic-16-reschedule"></i>7 days ago</div>
+def job_filter():
+    html_text = requests.get(
+        'https://internshala.com/internships/keywords-python/?utm_source=hp_internship_keyword_search').text
 
+    soup = BeautifulSoup(html_text, features="html.parser")
+    jobs = soup.find_all('div', class_="internship_meta")
+
+    for job in jobs:
+        get_time = job.find('div', class_="status status-small status-success")
+        # get_company_name = job.find('a', class_="link_display_like_text")
+        get_job_type = job.find('div', class_="company")
+        if get_time is not None:
+            get_job_type = get_job_type.text.replace(' \n', '')
+            get_time = get_time.text.replace(' \n', '')
+            print(f"""
+            job type = {get_job_type.strip()}
+            time posted = {get_time.strip()}
+            """)
+
+
+if __name__ == "__main__":
+    while True:
+        job_filter()
+        time.sleep(600)
